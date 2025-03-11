5f085b92370d670bbbc0d7bcb35b1682f90a1388
wildandr <wildandzakyramadhani@mail.com>
2025-03-12 03:31:45 +0700

    push

--- app.py
+++ app.py
@@ -252,4 +252,4 @@ def index():
     return render_template('index.html', date=datetime.now().strftime("%Y-%m-%d"))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
